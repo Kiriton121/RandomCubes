830a5cd70c71cfba23e1eff68433012b839fbc4b
Kiriton121 <98354579+Kiriton121@users.noreply.github.com>
2022-01-24 15:11:46 -0600

    Spam

---
+++ RandomCubesUnity/Assets/Scripts/RandomCubes.cs
@@ -0,0 +1,28 @@
+/****
+ * Created By: Zhiyong Lu
+ * Date Created: Jan 24, 2022
+ * 
+ * Last Edited By: Zhiyong Lu
+ * Last Edited: Jan24, 2022
+ * 
+ * Description: Spam 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCubes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
